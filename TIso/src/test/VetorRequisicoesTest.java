4eeed7baf0bd9d4b944bb9c87a128cb4cf41c1ee
indraAraujo <indra.araujo.santos@gmail.com>
2021-09-18 22:55:17 -0300

    feat: tests

--- TIso/src/test/VetorRequisicoesTest.java
+++ TIso/src/test/VetorRequisicoesTest.java
@@ -1,18 +1,34 @@
 package test;
-/*
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 
+import tiso.GeradorDeRequisitos;
 import tiso.Requisicao;
 import tiso.VetorRequisicoes;
 
 public class VetorRequisicoesTest {
-    int tamanho = 10;
-    VetorRequisicoes vr = new VetorRequisicoes(tamanho);
+    int tamanho_maximo_variavel = 1000;
+    int tamanho_minimo_variavel = 30;
+    GeradorDeRequisitos gerador = new GeradorDeRequisitos(tamanho_maximo_variavel, tamanho_minimo_variavel);
+    //TESTE DO VETOR DE REQUISIÇÕES
+    int tamanho_vetor = 100;
+    VetorRequisicoes vetorRequisicoes = new VetorRequisicoes(tamanho_vetor);
 
-    @Test 
     public void inserir(){
-        assertTrue((vr.inserir(new Requisicao(10))));
+         //Inserir no vetor de requisições uma requisição normal
+        vetorRequisicoes.inserir(gerador.gerarRequisicao());
+        vetorRequisicoes.inserir(gerador.gerarRequisicao());
+        vetorRequisicoes.inserir(gerador.gerarRequisicao());
+        vetorRequisicoes.inserir(gerador.gerarRequisicao());
+        vetorRequisicoes.inserir(gerador.gerarRequisicao());
+
     }
+
+    public void remover(){
+        //Remover variáveis
+        vetorRequisicoes.remover();
+        vetorRequisicoes.remover();
+        vetorRequisicoes.remover();
+        vetorRequisicoes.getVetor();
+    }
+   
+    
 }
-*/
